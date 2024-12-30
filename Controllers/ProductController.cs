f4bb86e5d689d289d7e38ec55ccedcf740f5299c
AdhyatmCodes27 <98180845+AdhyatmCodes27@users.noreply.github.com>
2024-12-30 15:44:02 +0530

    Added pagination to ProductController.cs

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProductApplication.Data;
+using ProductApplication.Models;
+
+namespace ProductApplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        public IActionResult Index(int page = 1, int pageSize = 10)
+        {
+         
+            int totalProducts = db.Products.Count();
+
+            int totalPages = (int)Math.Ceiling(totalProducts / (double)pageSize);
+
+            var products = db.Products
+                              .Include("Category") 
+                              .OrderBy(p => p.ProductId) 
+                              .Skip((page - 1) * pageSize)
+                              .Take(pageSize)             
+                              .ToList();
+
+            // pagination added
+
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = totalPages;
+            ViewBag.PageSize = pageSize;
+
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.Categories = new SelectList(db.Categories, "CategoryId", "CategoryName");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Categories = new SelectList(db.Categories, "CategoryId", "CategoryName", product.CategoryId);
+            return View(product);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var product = db.Products.Find(id);
+            ViewBag.Categories = new SelectList(db.Categories, "CategoryId", "CategoryName", product.CategoryId);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Categories = new SelectList(db.Categories, "CategoryId", "CategoryName", product.CategoryId);
+            return View(product);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var product = db.Products.Find(id);
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult ConfirmDelete(int id)
+        {
+            var product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
